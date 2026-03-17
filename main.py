6c9676ba977c181316a0e199917ca76df9d72f90
Fouzi Ibrahim <fouziibrahim06@gmail.com>
2026-03-17 21:59:59 +0000

    Mise à jour de data.json

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+from datetime import datetime
 schedule = []
 
 # Charger les données depuis data.json
@@ -16,6 +16,27 @@ def load_data():
 def save_data():
     with open("data.json", "w") as file:
         json.dump(schedule, file, indent=4)
+def has_conflict(day, start, end, ignore_index=None):
+
+    start_time = datetime.strptime(start, "%H:%M")
+    end_time = datetime.strptime(end, "%H:%M")
+
+    for i, course in enumerate(schedule):
+
+        # Ignorer le cours qu'on modifie
+        if i == ignore_index:
+            continue
+
+        if day.lower() == course["day"].lower():
+
+            course_start = datetime.strptime(course["start"], "%H:%M")
+            course_end = datetime.strptime(course["end"], "%H:%M")
+
+            # Vérifier chevauchement
+            if not (end_time <= course_start or start_time >= course_end):
+                return True
+
+    return False
 
 # Ajouter un cours
 def add_course():
@@ -32,7 +53,9 @@ def add_course():
         "end": end,
         "room": room
     }
-
+    if has_conflict(day, start, end):
+        print("Conflit d'horaire détecté. Cours non ajouté.")
+        return
     schedule.append(course)
     save_data()
     print("Cours ajouté avec succès !")
@@ -51,7 +74,7 @@ def delete_course():
     show_schedule()
     try:
         index = int(input("Numéro du cours à supprimer : ")) - 1
-        if 0 <= index < len(schedule):
+        if index >= 0 and index < len(schedule):
             schedule.pop(index)
             save_data()
             print("Cours supprimé.")
@@ -60,16 +83,64 @@ def delete_course():
     except ValueError:
         print("Entrée invalide.")
 
+# Rechercher les cours par jour #
+def searchCoursesByDay():
+    day = input("Entrez le jour pour rechercher les cours : ")
+    found = False
+    for course in schedule:
+        if course['day'].lower() == day.lower():
+            print(f"{course['day']} | {course['start']}-{course['end']} | {course['subject']} | Salle {course['room']}")
+            found = True
+    if not found :
+        print("Aucun cours trouvé pour ce jour.")
+
+# modifier un cours
+def modify_course():
+    show_schedule()
+    try:
+        index = int(input("Numéro de cours à modifier : ")) - 1
+        if index>= 0 and index < len(schedule):
+            old_course = schedule[index]
+            subject = input(f"entrer la nouvelle matière (actuelle : {old_course['subject']}): ")
+            day = input(f"entrer le nouveau jour (actuel : {old_course['day']}): ")
+            start = input(f"entrer la nouvelle heure de début (actuelle : {old_course['start']}): ")
+            end = input(f"entrer la nouvelle heure de fin (actuelle : {old_course['end']}): ")
+            room = input(f"entrer la nouvelle salle (actuelle : {old_course['room']}): ")
+            new_course = {
+                "subject": subject if subject else old_course['subject'],
+                "day": day if day else old_course['day'],
+                "start": start if start else old_course['start'],
+                "end": end if end else old_course['end'],
+                "room": room if room else old_course['room']
+            }
+            if has_conflict(new_course['day'], new_course['start'], new_course['end'], index):
+                print("Conflit d'horaire détecté. Cours non modifié.")
+                return
+            schedule[index] = new_course
+            save_data()
+            print("Cours modifié avec succès !")
+        else:
+            print("Numéro invalide.")
+    except ValueError:
+        print("Entrée invalide.")
+        
 # Menu principal
 def menu():
     while True:
-        print("\n===== EMPLOI DU TEMPS =====")
-        print("1. Ajouter un cours")
-        print("2. Afficher emploi du temps")
-        print("3. Supprimer un cours")
-        print("4. Quitter")
+        print("\n" + "="*40)
+        print("        EMPLOI DU TEMPS")
+        print("="*40)
 
-        choice = input("Choix : ")
+        print(" 1) Ajouter un cours")
+        print(" 2) Afficher l'emploi du temps")
+        print(" 3) Supprimer un cours")
+        print(" 4) Rechercher par jour")
+        print(" 5) Modifier un cours")
+        print(" 0) Quitter")
+
+        print("-"*40)
+
+        choice = input("Entrez votre choix : ")
 
         if choice == "1":
             add_course()
@@ -78,11 +149,14 @@ def menu():
         elif choice == "3":
             delete_course()
         elif choice == "4":
+            searchCoursesByDay()
+        elif choice == "5":
+            modify_course()
+        elif choice == "0":
             print("Au revoir !")
             break
         else:
-            print("Choix invalide.")
-
+            print("❌ Choix invalide.")
 # Lancer programme
 load_data()
 menu()
